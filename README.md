6ec8730f8f4b9b80c943769282ad1fd2c54ddf07
Xuanwo <github@xuanwo.io>
2021-07-05 13:54:27 +0800

    Add support for setup hdfs
    Signed-off-by: Xuanwo <github@xuanwo.io>

---
+++ README.md
@@ -0,0 +1,7 @@
+This action sets up Apache Hadoop/HDFS in your environment for use in GitHub Actions.
+
+## Statement
+
+This project is highly inspired by [setup-spark](https://github.com/vemonet/setup-spark).
+
+Thanks to [@vemonet](https://github.com/vemonet/setup-spark).
---
+++ src/setup-hdfs.ts
@@ -0,0 +1,82 @@
+import * as core from '@actions/core';
+import {exec} from 'child_process';
+import * as fs from 'fs';
+
+
+function setup() {
+    const hdfsVersion = core.getInput('hdfs-version');
+
+    let installFolder: any = process.env.GITHUB_WORKSPACE + '/../'
+    fs.access(installFolder, fs.constants.W_OK, (err) => {
+        console.log('$GITHUB_WORKSPACE parent not writable. Using $GITHUB_WORKSPACE to store Spark');
+        installFolder = process.env.GITHUB_WORKSPACE
+    });
+
+    // Full list here: http://www.apache.org/mirrors/
+    //
+    // TODO: maybe we need to support user provided download url.
+    const hdfsUrl = `https://mirrors.gigenet.com/apache/hadoop/core/hadoop-${hdfsVersion}/hadoop-${hdfsVersion}.tar.gz`;
+
+    // Download and setup hadoop.
+    var command = `cd /tmp &&
+  wget -q -O hdfs.tgz ${hdfsUrl} &&
+  tar xzf hdfs.tgz -C ${installFolder} &&
+  rm "hdfs.tgz"
+  ln -s "${installFolder}/hadoop-${hdfsVersion}" ${installFolder}/hdfs`
+
+    exec(command, (err: any, stdout: any, stderr: any) => {
+        if (err || stderr) {
+            console.log('Error downloading the Spark binary');
+            throw new Error(err);
+        }
+    });
+
+    // Configure hdfs.
+    const hdfsHome = installFolder + '/hdfs';
+
+    const core_site = `<configuration>
+    <property>
+        <name>fs.defaultFS</name>
+        <value>hdfs://localhost:9000</value>
+    </property>
+</configuration>`
+    exec("echo ${core_site} > ${hdfsHome}/etc/hadoop/core-site.xml", (err: any, stdout: any, stderr: any) => {
+        if (err || stderr) {
+            console.log('Error setup core-site.xml');
+            throw new Error(err);
+        }
+    })
+    const hdfs_site = `<configuration>
+    <property>
+        <name>dfs.replication</name>
+        <value>1</value>
+    </property>
+</configuration>`
+    exec("echo ${hdfs_site} > ${hdfsHome}/etc/hadoop/hdfs-site.xml", (err: any, stdout: any, stderr: any) => {
+        if (err || stderr) {
+            console.log('Error setup hdfs-site.xml');
+            throw new Error(err);
+        }
+    })
+
+    // Start hdfs daemon.
+    exec("${hdfsHome}/bin/hdfs namenode -format", (err: any, stdout: any, stderr: any) => {
+        if (err || stderr) {
+            console.log('Error format hdfs namenode');
+            throw new Error(err);
+        }
+    })
+    exec("${hdfsHome}/sbin/start-dfs.sh", (err: any, stdout: any, stderr: any) => {
+        if (err || stderr) {
+            console.log('Error start-dfs');
+            throw new Error(err);
+        }
+    })
+}
+
+try {
+    setup()
+} catch (error) {
+    console.log(error);
+    core.setFailed(error.message);
+}
